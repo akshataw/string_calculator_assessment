101e8e72d90718109ed5c542d5781608c8a48872
udakshataw <akshata.waghe@udgama.com>
2025-04-05 13:16:32 +0530

    Update: custom delimiters

--- calculator.rb
+++ calculator.rb
@@ -1,5 +1,19 @@
 class StringCalculator
     def add(str_input)
         return 0 if str_input.strip.empty?
+        delimiters = /,|\n/
+
+        if str_input.start_with?("//")
+            initials, str_input = str_input.split("\n")
+            puts "Initials: #{initials} & input: #{str_input}"
+            custom_delimiters = initials[2..]
+            puts "Custom delimiters: #{custom_delimiters}"
+            delimiters = Regexp.new(Regexp.escape(custom_delimiters))
+            puts "New delimiters: #{delimiters}"
+        end
+
+        numbers = str_input.split(delimiters).map(&:to_i)
+        puts "Numbers: #{numbers}"
+        numbers.sum
     end
 end
--- calculator_spec.rb
+++ calculator_spec.rb
@@ -6,4 +6,24 @@ RSpec.describe StringCalculator do
     it "return 0 for empty string" do
         expect(calc.add("")).to eq(0)
     end
+
+    it "returns number for single value" do
+        expect(calc.add("1")).to eq(1)
+    end
+
+    it "returns sum of two numbers" do
+        expect(calc.add("1,5")).to eq(6)
+    end
+
+    it "returns sum of all numbers" do
+        expect(calc.add("1,2,3,4,5")).to eq(15)
+    end
+
+    it "returns sum of all numbers with new line delimiter" do
+        expect(calc.add("1\n2,3")).to eq(6)
+    end
+
+    it "returns sum of number removing all delimiters" do
+        expect(calc.add("//;\n1;2")).to eq(3)
+    end
 end

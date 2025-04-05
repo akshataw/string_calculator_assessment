cbe7c61dd8bcee36f5a34dd62ea8c727ec25ae62
udakshataw <akshata.waghe@udgama.com>
2025-04-05 12:32:32 +0530

    Update: Initial test for empty string

---
+++ calculator_spec.rb
@@ -0,0 +1,9 @@
+require './calculator'
+
+Rspec.describe StringCalculator do
+    let(:calc) { StringCalculator.new }
+
+    it "return 0 for empty string" do
+        expect(calc.add("")).to eq(0)
+    end
+end
